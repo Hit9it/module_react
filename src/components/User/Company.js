2052e40c76208d2ebe1d90ec5e5acad2d531cf43
Hit9it <hit9it@gmail.com>
2021-08-17 19:29:33 +0300

    Homework

---
+++ src/components/User/Company.js
@@ -0,0 +1,7 @@
+export default function Company({name,catchPhrase,bs}){
+    return(
+        <div>
+            <h4>Company {name}</h4>
+            <div>catchPhrase: {catchPhrase}<br/> bs: {bs}</div>
+        </div>
+    );}
