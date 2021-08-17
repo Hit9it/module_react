a15daa18b7d145e433add12b81e15767dd9e047c
Hit9it <hit9it@gmail.com>
2021-08-17 19:26:01 +0300

    Homework

--- src/components/User/Address.js
+++ src/components/User/Address.js
@@ -1,3 +1,11 @@
-export function Address(street,street,zipcode,geo){
-    <h4>Address:</h4>
+import Geo from "../geo/geo";
+
+export default function Address({street, suite, city, zipcode, geo}) {
+    return (
+        <div>
+            <h4>address:</h4>
+            <div>street: {street}<br/> suite: {suite}<br/> city: {city}<br/> zipcode: {zipcode}</div>
+            <Geo{...geo}/>
+        </div>
+    );
 }
